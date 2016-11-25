83d71f16f18191840aec9b336163db96b9e61906
ghzh <zhangguohua02@baidu.com>
2016-11-25 15:58:37 +0800

    mp3dec init version

--- IoT_COPY_FILE.py
+++ IoT_COPY_FILE.py
@@ -9,6 +9,9 @@ import re
 from pprint import pprint
 
 
+def copy_fileto(src, dst_dir):
+    shutil.copy(src, dst_dir)
+
 def getCurrentTime():
     nowTime = time.localtime()
     year = str(nowTime.tm_year)
@@ -36,20 +39,30 @@ def search_and_copy_file_by_name(search_dir, file_name, copy_dir):
         print 'search_dir not exit'
         retunr -1
     for file in os.listdir(search_dir):
-        target_file = os.path.join(search_dir, file)
+        src_file = os.path.join(search_dir, file)
         #print target_file
         #print os.path.isfile(target_file)
         exclude = '.*?(uvoptx|uvprojx)$'
-        if os.path.isfile(target_file) and re.match(file_name, file) and not re.match(exclude, target_file):
+        if os.path.isfile(src_file) and re.match(file_name, file) and not re.match(exclude, src_file):
             if not os.path.exists(copy_dir):
-                os.makedirs(copy_dir)
-            dst_file = os.path.join(copy_dir, file)
-            #print dst_file
-            #print target_file
-            open(dst_file, "wb").write(open(target_file,"rb").read())
+               os.makedirs(copy_dir)
 
-        if os.path.isdir(target_file):
-            search_and_copy_file_by_name(target_file, file_name, copy_dir)
+            dst_file = os.path.join(copy_dir, file)
+            print dst_file
+            print os.path.isfile(dst_file)
+            try:
+                if os.path.exists(src_file) and os.path.isfile(src_file):
+                    print os.path.getsize(src_file)
+                    if not os.path.exists(dst_file) or (os.path.exists(dst_file) and os.path.getsize(src_file) !=
+                    os.path.getsize(dst_file) ):
+                        print dst_file
+                        print src_file
+                        open(dst_file, "wb").write(open(src_file,"rb").read()) #should close file when use
+            except OSError:
+                print src_file + ' error'
+                return -2
+        if os.path.isdir(src_file):
+            search_and_copy_file_by_name(src_file, file_name, copy_dir)
 
 
 """
@@ -68,7 +81,13 @@ def remove_file(dir, key):
 
 if __name__ == "__main__":
     dirTime = getCurrentTime()
+    src_dir = 'E:\\ref\\python\\IoTImage\\projects'
+    src = 'E:\\ref\\python\\IoTImage\\projects\\clean-obj.py'
     copy_dir = 'E:\\ref\\python\\IoTImage' + dirTime
     #print copy_dir
-    search_and_copy_file_by_name('E:\\ref\\python\\IoTImage\\projects', 'uv4_log|TinyDu*', copy_dir)
+    search_and_copy_file_by_name(src_dir, 'uv4_log|TinyDu*', copy_dir)
     #remove_file(copy_dir, '.*?(uvoptx|uvprojx|BAT|bat)$')
+    #try:
+    print copy_fileto(src,'E:\\ref\\python')
+    #except IOError:
+    print 'move file to IOerror'
